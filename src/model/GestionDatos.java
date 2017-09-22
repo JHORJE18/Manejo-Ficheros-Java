5a080bd8dc53e752a5a2fb6f216902567f374809
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-22 16:17:09 +0200

    Abrir y cerrar fichero
    Usando BufferedReader

--- src/model/GestionDatos.java
+++ src/model/GestionDatos.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.io.File;
+import java.io.*;
 
 public class GestionDatos {
 		
@@ -9,17 +9,43 @@ public class GestionDatos {
 	}
 
 	//TODO: Implementa una funci�n para abrir ficheros
-	public void abrirFichero(String fichero){
-
+	public BufferedReader abrirFichero(String fichero) {
+		try {
+			FileReader fileR = new FileReader(fichero);
+			BufferedReader fileBR = new BufferedReader(fileR);
+			return fileBR;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al abrir el archivo " + fichero);
+			return null;
+		}
 	}
 	
 	//TODO: Implementa una funci�n para cerrar ficheros
-	public void cerrarFichero(String fichero){
-		
+	public void cerrarFichero(BufferedReader fichero) {
+		try {
+			fichero.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("No se ha podido cerrar el fichero");
+		}
 	}
+	
+	/*
+	 * public static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
+        String cadena;
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+            System.out.println(cadena);
+        }
+        b.close();
+    	}
+	 */
 	
 	public boolean compararContenido (String fichero1, String fichero2){
 		//TODO: Implementa la funci�n
+		
 		return true;
 	}
 	
