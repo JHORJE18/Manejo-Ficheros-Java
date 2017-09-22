686a4453de41d7da3a8bb48405c3aed117bc97ae
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-22 15:56:48 +0200

    Crear metodos

--- src/controller/GestionEventos.java
+++ src/controller/GestionEventos.java
@@ -20,14 +20,14 @@ public class GestionEventos {
 	public void contol() {
 		actionListener_comparar = new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
-				// TODO: Llamar a la función call_compararContenido
+				// TODO: Llamar a la funciï¿½n call_compararContenido
 			}
 		};
 		view.getComparar().addActionListener(actionListener_comparar);
 
 		actionListener_buscar = new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
-				// TODO: Llamar a la función call_buscarPalabra
+				// TODO: Llamar a la funciï¿½n call_buscarPalabra
 			}
 		};
 		view.getBuscar().addActionListener(actionListener_buscar);
@@ -35,14 +35,14 @@ public class GestionEventos {
 
 	private int call_compararContenido() {
 
-		// TODO: Llamar a la función compararContenido de GestionDatos
+		// TODO: Llamar a la funciï¿½n compararContenido de GestionDatos
 		// TODO: Gestionar excepciones
 		return 1;
 	}
 
 	private int call_buscarPalabra() {
 
-		// TODO: Llamar a la función buscarPalabra de GestionDatos
+		// TODO: Llamar a la funciï¿½n buscarPalabra de GestionDatos
 		// TODO: Gestionar excepciones
 		return 1;
 	}
--- src/model/GestionDatos.java
+++ src/model/GestionDatos.java
@@ -1,22 +1,30 @@
 package model;
 
+import java.io.File;
+
 public class GestionDatos {
-
+		
 	public GestionDatos() {
-
+		
 	}
 
-	//TODO: Implementa una función para abrir ficheros
+	//TODO: Implementa una funciï¿½n para abrir ficheros
+	public void abrirFichero(String fichero){
+
+	}
 	
-	//TODO: Implementa una función para cerrar ficheros
+	//TODO: Implementa una funciï¿½n para cerrar ficheros
+	public void cerrarFichero(String fichero){
+		
+	}
 	
 	public boolean compararContenido (String fichero1, String fichero2){
-		//TODO: Implementa la función
+		//TODO: Implementa la funciï¿½n
 		return true;
 	}
 	
 	public int buscarPalabra (String fichero1, String palabra, boolean primera_aparicion){
-		//TODO: Implementa la función
+		//TODO: Implementa la funciï¿½n
 		return 1;
 	}	
 
